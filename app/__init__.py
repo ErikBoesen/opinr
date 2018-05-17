10eb67cec4ccc6b9be98b206475665733d49ca44
ErikBoesen <me@erikboesen.com>
2018-05-17 13:53:33 -0400

    Use production config

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #Configuration of application, see configuration.py, choose one and uncomment.
-#app.config.from_object('configuration.ProductionConfig')
-app.config.from_object('app.configuration.DevelopmentConfig')
+app.config.from_object('app.configuration.ProductionConfig')
+#app.config.from_object('app.configuration.DevelopmentConfig')
 #app.config.from_object('configuration.TestingConfig')
 app.config.setdefault('CACHE_TYPE', 'null')
 

d023757a777bd7c5559645c2bba7240b549bb396
ErikBoesen <me@erikboesen.com>
2018-09-29 18:31:41 -0400

    Add comment clarifying nonuse of Mongo

--- app/views.py
+++ app/views.py
@@ -5,6 +5,9 @@ import json
 
 @app.route('/')
 def index():
+    # It would probably be more professional to serve data from a Mongo database
+    # or something like that, but given the scale of this application I think
+    # it's okay to just serve from a JSON file.
     with open('app/data/issues.json', 'r') as f:
         issues = json.load(f)
     return render_template('index.html', issues=issues)
